25c6faf851505cda788d1ee2f6f281bc5671ae1f
yame123 <inyasang@gmail.com>
2023-09-15 10:54:13 +0900

    init commit

---
+++ src/main/java/com/sparta/team14project/Team14projectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.team14project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team14projectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team14projectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/team14project/Team14projectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.team14project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team14projectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
